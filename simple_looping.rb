8a0bf11e689c41b48101a2a01b1590ed4883b9f5
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-12 19:08:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,11 +1,11 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  number_of_times = 0
+  times = 0
   loop do 
-    number_of_times += 1
+    times += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    if number_of_times >= 7
+    if times >= number_of_times
       break
     end
   end
